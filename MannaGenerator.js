121311236f08d25c7488207cfaa36009085deace
Rob Bishop <GreatBigBore@gmail.com>
2016-10-22 14:03:41 -0700

    Refactoring, prep for seasonal manna

--- MannaGenerator.js
+++ MannaGenerator.js
@@ -24,34 +24,87 @@ if(typeof window === "undefined") {
 
 (function(Archonia) {
   
+  var arrayScale = null;
+  var bellCurve = null;
+  var bellCurveHeight = 5;
+  var bellCurveRadius = null;
+  var gameScale = null;
+  var morselScale = 0.05;
   var started = false;
+  var tempScale = null;
+
+  var giveth = function() {
+    var thisParticle = null;
+    var temp = Archonia.Cosmos.Sun.getTemperature(Archonia.Essence.gameCenter);
+    
+    for(var i = 0; i < 10; i++) {
+      var rp = new Archonia.Form.RandomXY();
+      rp.setMin(0, 0);
+      rp.setMax(Archonia.Axioms.gameWidth, Archonia.Axioms.gameHeight);
+      rp.random();
+      
+      var scaledY = arrayScale.convertPoint(rp.point.y, gameScale);
+      var p = bellCurve[Math.floor(scaledY)] / bellCurveHeight;
+      
+      if(Archonia.Axioms.realInRange(0, 1) < p) {
+
+        thisParticle = Archonia.Cosmos.MannaGenerator.spriteGroup.getFirstDead();
+        
+        if(thisParticle === null) {
+          break;
+        } else {
+          rp.point.y += gameScale.convertPoint(temp, tempScale);
+          rp.point.floor();
+        
+          if(rp.point.isInBounds()) {
+            thisParticle.archoniaUniqueObjectId = Archonia.Essence.archoniaUniqueObjectId++;
+            thisParticle.body.position.setTo(rp.point.x, rp.point.y);
+            thisParticle.position.setTo(rp.point.x, rp.point.y);
+            thisParticle.reset(rp.point.x, rp.point.y);
+            thisParticle.revive();
+          }
+        }
+        
+      }
+    }
+  };
   
-  var MG = Archonia.Cosmos.MannaGenerator = {
+  var takethAway = function() {
+    for(var i = 0; i < 10; i++) {
+      var thisParticle = Archonia.Cosmos.MannaGenerator.spriteGroup.getRandom();
+      if(thisParticle.alive) {
+        var r = Archonia.Axioms.integerInRange(0, bellCurve.length);
+        var p = bellCurve[r] / bellCurveHeight;
+  
+        if(Archonia.Axioms.realInRange(0, 1) < p) {
+          thisParticle.kill();
+        }
+      }
+    }
+  };
+  
+  Archonia.Cosmos.MannaGenerator = {
+    frameCount: null,
+
     initialize: function() {
-      var morselScale = 0.05;
-    
-      MG.howManyMorselsLaunched = 0;
-      MG.morselIndex = null;
-    
-      MG.bellCurveHeight = 5;
     
       var stopBelow = 0.01, xOffset = 0, width = 2;
-      MG.bellCurve = Archonia.Axioms.generateBellCurve(stopBelow, MG.bellCurveHeight, xOffset, width);
-      if(MG.bellCurve.length % 2 === 1) { MG.bellCurve.push(0); }
-      MG.bellCurveRadius = MG.bellCurve.length / 2;
+      bellCurve = Archonia.Axioms.generateBellCurve(stopBelow, bellCurveHeight, xOffset, width);
+      if(bellCurve.length % 2 === 1) { bellCurve.push(0); }
+      bellCurveRadius = bellCurve.length / 2;
     
       // We make the A.game scale larger than the radius so the manna will go off
       // the screen when the temps are extreme in either direction
-      MG.tempScale = new Archonia.Form.Range(-1000, 1000);
-      MG.gameScale = new Archonia.Form.Range(-Archonia.Axioms.gameRadius, Archonia.Axioms.gameRadius);
-      MG.arrayScale = new Archonia.Form.Range(-MG.bellCurveRadius, MG.bellCurveRadius);
+      tempScale = new Archonia.Form.Range(-1000, 1000);
+      gameScale = new Archonia.Form.Range(-Archonia.Axioms.gameRadius, Archonia.Axioms.gameRadius);
+      arrayScale = new Archonia.Form.Range(-bellCurveRadius, bellCurveRadius);
     
-      MG.spriteGroup = Archonia.Engine.game.add.group();
-      MG.spriteGroup.enableBody = true;
-      MG.spriteGroup.createMultiple(Archonia.Axioms.howManyMannaMorsels, 'particles', 0, false);
-      Archonia.Engine.game.physics.enable(MG.spriteGroup, Phaser.Physics.ARCADE);
+      Archonia.Cosmos.MannaGenerator.spriteGroup = Archonia.Engine.game.add.group();
+      Archonia.Cosmos.MannaGenerator.spriteGroup.enableBody = true;
+      Archonia.Cosmos.MannaGenerator.spriteGroup.createMultiple(Archonia.Axioms.howManyMannaMorsels, 'particles', 0, false);
+      Archonia.Engine.game.physics.enable(Archonia.Cosmos.MannaGenerator.spriteGroup, Phaser.Physics.ARCADE);
 
-      MG.spriteGroup.forEach(function(m) {
+      Archonia.Cosmos.MannaGenerator.spriteGroup.forEach(function(m) {
         m.archoniaUniqueObjectId = Archonia.Essence.archoniaUniqueObjectId++;
         m.calories = Archonia.Axioms.caloriesPerManna;
         m.previousEmit = 0;
@@ -66,58 +119,8 @@ if(typeof window === "undefined") {
         m.tint = 0x5C008E;
       }, this);
     },
-  
-    giveth: function() {
-      var thisParticle = null;
-      var temp = Archonia.Cosmos.Sun.getTemperature(Archonia.Essence.gameCenter);
-      
-      for(var i = 0; i < 10; i++) {
-        var rp = new Archonia.Form.RandomXY();
-        rp.setMin(0, 0);
-        rp.setMax(Archonia.Axioms.gameWidth, Archonia.Axioms.gameHeight);
-        rp.random();
-        
-        var scaledY = MG.arrayScale.convertPoint(rp.point.y, MG.gameScale);
-        var p = MG.bellCurve[Math.floor(scaledY)] / MG.bellCurveHeight;
-        
-        if(Archonia.Axioms.realInRange(0, 1) < p) {
-
-          thisParticle = MG.spriteGroup.getFirstDead();
-          
-          if(thisParticle === null) {
-            break;
-          } else {
-            rp.point.y += MG.gameScale.convertPoint(temp, MG.tempScale);
-            rp.point.floor();
-          
-            if(rp.point.isInBounds()) {
-              thisParticle.archoniaUniqueObjectId = Archonia.Essence.archoniaUniqueObjectId++;
-              thisParticle.body.position.setTo(rp.point.x, rp.point.y);
-              thisParticle.position.setTo(rp.point.x, rp.point.y);
-              thisParticle.reset(rp.point.x, rp.point.y);
-              thisParticle.revive();
-            }
-          }
-          
-        }
-      }
-    },
     
     start: function() { started = true; },
-    
-    takethAway: function() {
-      for(var i = 0; i < 10; i++) {
-        var thisParticle = MG.spriteGroup.getRandom();
-        if(thisParticle.alive) {
-          var r = Archonia.Axioms.integerInRange(0, MG.bellCurve.length);
-          var p = MG.bellCurve[r] / MG.bellCurveHeight;
-    
-          if(Archonia.Axioms.realInRange(0, 1) < p) {
-            thisParticle.kill();
-          }
-        }
-      }
-    },
     
     render: function() {
       if(!started) { return; }
@@ -125,7 +128,7 @@ if(typeof window === "undefined") {
     	var showDebugOutlines = false;
 
     	if(showDebugOutlines) {
-    		MG.spriteGroup.forEachAlive(function(a) {
+    		Archonia.Cosmos.MannaGenerator.spriteGroup.forEachAlive(function(a) {
     	    Archonia.Engine.game.debug.body(a, 'yellow', false);
     			Archonia.Engine.game.debug.spriteBounds(a, 'blue', false);
     		}, this);
@@ -135,9 +138,9 @@ if(typeof window === "undefined") {
     tick: function(frameCount) {
       if(!started) { return; }
       
-      MG.frameCount = frameCount;
-      MG.giveth();
-      MG.takethAway();
+      Archonia.Cosmos.MannaGenerator.frameCount = frameCount;
+      giveth();
+      takethAway();
     }
     
   };
