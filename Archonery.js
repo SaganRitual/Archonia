14cbc98cf8667ddc51801c03c997dc2e4670a72e
Rob Bishop <GreatBigBore@gmail.com>
2016-10-30 15:26:10 -0700

    Reorg

---
+++ Archonery.js
@@ -0,0 +1,13 @@
+/* jshint forin:false, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, loopfunc:true,
+	undef:true, unused:true, curly:true, browser:true, indent:false, maxerr:50, jquery:true, node:true */
+
+"use strict";
+
+// jshint ignore: start
+var Archonia = Archonia || { Axioms: {}, Cosmos: {}, Engine: {}, Essence: {}, Form: {} };
+
+(function(Archonia) {
+  
+})(Archonia);
+
+// jshint ignore: end
