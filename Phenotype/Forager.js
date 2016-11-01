b38337c2a7e82f16f33f936f367cbf4874da8912
Rob Bishop <GreatBigBore@gmail.com>
2016-10-31 23:47:55 -0700

    Checkpoint; trying to work out forage directions

--- Phenotype/Forager.js
+++ Phenotype/Forager.js
@@ -47,8 +47,8 @@ Archonia.Form.Forager = function(archon) {
   
   if(yAxisRange === null) {
     yAxisRange = new Archonia.Form.Range(
-      Archonia.Engine.game.world.centerY + Archonia.Axioms.gameRadius,
-      Archonia.Engine.game.world.centerY - Archonia.Axioms.gameRadius
+      Archonia.Engine.game.world.centerY - Archonia.Axioms.gameRadius,
+      Archonia.Engine.game.world.centerY + Archonia.Axioms.gameRadius
     );
   }
 };
@@ -60,36 +60,27 @@ Archonia.Form.Forager.prototype = {
     var netTemp = Math.abs(tempSignal) * this.genome.tempToleranceMultiplier;
     var netHunger = hungerSignal * this.genome.hungerToleranceMultiplier;
     
-    if(this.state.archonUniqueId === 0) {
-      Archonia.Essence.Logger.log(
-        tempSignal.toFixed(2), hungerSignal.toFixed(2), ySignal.toFixed(2),
-        netTemp.toFixed(2), netHunger.toFixed(2), this.state.position.toString()
-      );
-    }
-    
     if(netTemp > netHunger) {
       // If temp wins, then we just go in the direction that gets us
       // closer to a temp signal of zero
       if(tempSignal > 0) { this.where = "randomDownOnly"; } else { this.where = "randomUpOnly"; }
       
     } else {
-      // Note: when we compare the tempSignal to the ySignal, but that doesn't
-      // really work in principle. The temp signal is about our own health; what
-      // I'm trying to do here is get us to go to where the manna is most
-      // likely to be. Won't work if our own temp signal doesn't correspond to
-      // the way the manna grows. Also I think it defeats the whole fat manna thing.
-      // Come back to this when not trying to fix a hundred other things
       var upOk = true, downOk = true;
-      if(Math.abs(ySignal) < Math.abs(tempSignal) && ySignal >= 0 && tempSignal < 0) { downOk = false; }
-      if(Math.abs(ySignal) < Math.abs(tempSignal) && ySignal <= 0 && tempSignal > 0) { upOk = false; }
-      if(Math.abs(ySignal) < Math.abs(tempSignal) && ySignal <= 0 && tempSignal < 0) { downOk = false; }
-      if(Math.abs(ySignal) < Math.abs(tempSignal) && ySignal >= 0 && tempSignal > 0) { upOk = false; }
-
-      if(Math.abs(ySignal) > Math.abs(tempSignal) && ySignal >= 0 && tempSignal < 0) { upOk = false; }
-      if(Math.abs(ySignal) > Math.abs(tempSignal) && ySignal <= 0 && tempSignal > 0) { downOk = false; }
-      if(Math.abs(ySignal) > Math.abs(tempSignal) && ySignal <= 0 && tempSignal < 0) { downOk = false; }
-      if(Math.abs(ySignal) > Math.abs(tempSignal) && ySignal >= 0 && tempSignal > 0) { upOk = false; }
       
+      // At this temp, no manna will be growing nearby; if we're
+      // above the x-axis, we need to rule out upward movement
+      if(tempSignal > 0.5 && ySignal < 0) { upOk = false; }
+      
+      if(tempSignal < 0.5 && ySignal > 0) { downOk = false; }
+      
+      if(this.state.archonUniqueId === 0) {
+        Archonia.Essence.Logger.log(
+          tempSignal.toFixed(2), hungerSignal.toFixed(2), ySignal.toFixed(2),
+          netTemp.toFixed(2), netHunger.toFixed(2), this.state.position.toString()
+        );
+      }
+
       if(upOk && downOk) { this.where = "random"; }
       else if(upOk)      { this.where = "randomUpOnly"; }
       else               { this.where = "randomDownOnly"; }
@@ -160,6 +151,7 @@ Archonia.Form.Forager.prototype = {
     this.searchAnchor.reset();
     this.trail.reset();
     this.currentMannaTarget = null;
+    this.where = "random";
   },
   
   tick: function() {
@@ -194,7 +186,7 @@ Archonia.Form.Forager.prototype = {
         var hungerSignal = this.state.hungerInput.getSignalStrength();
         this.computeFoodSearchState(tempSignal, hungerSignal);
 
-        this.forage("random");
+        this.forage(this.where);
         this.whenToIssueNextMove = this.state.frameCount + howManyTicksBetweenMoves;
       }
 
